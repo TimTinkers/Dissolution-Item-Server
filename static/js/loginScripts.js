10e232de62f2543257975384ace368dcdab88aad
Timothy Clancy <tim@rockhopper.us>
2019-06-29 20:44:15 -0400

    Updated login page to return a set of useful error messages.

--- static/js/loginScripts.js
+++ static/js/loginScripts.js
@@ -1,31 +1,20 @@
-// A helper which can seamlessly convert contract calls to Promises. Thanks to:
-// https://ethereum.stackexchange.com/questions/11444/web3-js-with-promisified-api/24238#24238
-// http://shawntabrizi.com/crypto/making-web3-js-work-asynchronously-javascript-promises-await/
-const promisify = (inner) =>
-new Promise((resolve, reject) =>
-	inner((err, res) => {
-		if (err) {
-			reject(err);
-		} else {
-			resolve(res);
-		}
-	})
-);
-
 // A function which asynchronously sets up the page.
 var setup = async function (config) {
 
-	// Poll the user's active wallet address every few seconds.
-	var update = async function () {
-	};
-	update();
-	setInterval(update, 2500);
+	// Check for an error message to display.
+	var errorMessage = window.serverData.error;
+	if (errorMessage) {
+		var errorBox = $('#errorBox');
+		errorBox.html(errorMessage);
+		errorBox.show();
+	}
 }
 
 // Once the window has fully loaded, begin page setup.
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
 
 	// Parse the configuration file and pass to setup.
-	$.getJSON("js/config.json", function (config) {
+	$.getJSON('js/config.json', function (config) {
+		setup(config);
 	});
 });
