86a9620cbf77e55e7d70c951962d699ebe02c6ee
Timothy Clancy <tim@rockhopper.us>
2019-06-29 20:44:33 -0400

    Added better error handling to the app.

--- app.js
+++ app.js
@@ -25,37 +25,54 @@ const asyncMiddleware = fn => (req, res, next) => {
 	.catch(next);
 };
 
-// Constants.
+// Constant for tracking the administrator's access token.
 var ADMIN_ACCESS_TOKEN;
 
 // Launch the application and begin the server listening.
-app.listen(3000, async function () {
-	console.log("Dissolution item exchange server listening on port 3000.");
+var server = app.listen(3000, async function () {
+	console.log('Dissolution item exchange server listening on port 3000.');
 
 	// Retrieve administrator credentials.
 	var adminUsername = process.env.ADMIN_USERNAME;
 	var adminPassword = process.env.ADMIN_PASSWORD;
-	const postData = JSON.stringify({
-		username : adminUsername,
-		password : adminPassword
-	});
 
-	var loginResponse = await request({
-		method: 'POST',
-		uri: 'https://api.dissolution.online/core/master/login/',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'Content-Length': postData.length
-		},
-		body: postData
-	});
-	loginResponse = JSON.parse(loginResponse);
+	// Verify that the administrator credentials were actually provided.
+	if (!adminUsername || !adminPassword) {
+		console.error('You must specify administrator credentials in .env!');
+		server.close();
+		return;
+	}
 
-	ADMIN_ACCESS_TOKEN = loginResponse['access_token'];
+	// Attempt to login with the administrator.
+	try {
+		const postData = JSON.stringify({
+			username : adminUsername,
+			password : adminPassword
+		});
+		var loginResponse = await request({
+			method: 'POST',
+			uri: 'https://api.dissolution.online/core/master/login/',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Content-Length': postData.length
+			},
+			body: postData
+		});
+		loginResponse = JSON.parse(loginResponse);
+
+		// Store the administrator's access token for later.
+		ADMIN_ACCESS_TOKEN = loginResponse['access_token'];
+
+	// Verify that we were actually able to login.
+	} catch (error) {
+		console.error('Unable to login as administrator. Check your credentials.');
+		server.close();
+		return;
+	}
 });
 
-// A helper function to verify the dissolution access token.
+// A helper function to verify the Dissolution access token.
 function getKey (header, callback) {
 	dissolutionClient.getSigningKey(header.kid, function (error, key) {
 		var signingKey = key.publicKey || key.rsaPublicKey;
@@ -65,13 +82,19 @@ function getKey (header, callback) {
 
 // Validate user login and handle appropriate routing.
 app.get('/', function (req, res) {
+
+	// If the user does not have a valid access cookie, prompt them for login.
 	var dissolutionToken = req.cookies.dissolutionToken;
 	if (dissolutionToken === undefined || dissolutionToken === 'undefined') {
-		res.render('login');
+		res.render('login', { error : 'null' });
+
+	// Otherwise, verify the correctness of the access token.
 	} else {
 		jwt.verify(dissolutionToken, getKey, function (error, decoded) {
 			if (error) {
-				res.render('login');
+				res.render('login', {
+					error : "\'Your access token cannot be verified. Please log in again.\'"
+				});
 			} else {
 				res.render('dashboard', decoded);
 			}
@@ -81,32 +104,48 @@ app.get('/', function (req, res) {
 
 // Handle visitors logging in through the web app.
 app.post('/login', asyncMiddleware(async (req, res, next) => {
+
+	// Retrieve the username and password from our request body.
 	var username = req.body.username;
 	var password = req.body.password;
 
-	const postData = JSON.stringify({
-		username : username,
-		password : password
-	});
+	// Return an appropriate error message if these fields are not provided.
+	if (!username || !password) {
+		res.render('login', { error : "\'You must provide valid login details.\'" });
+		return;
+	}
 
-	var loginResponse = await request({
-		method: 'POST',
-		uri: 'https://api.dissolution.online/core/master/login/',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'Content-Length': postData.length
-		},
-		body: postData
-	});
-	loginResponse = JSON.parse(loginResponse);
+	// Otherwise, attempt to log the user in.
+	try {
+		const postData = JSON.stringify({
+			username : username,
+			password : password
+		});
+		var loginResponse = await request({
+			method: 'POST',
+			uri: 'https://api.dissolution.online/core/master/login/',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Content-Length': postData.length
+			},
+			body: postData
+		});
+		loginResponse = JSON.parse(loginResponse);
 
-	var accessToken = loginResponse['access_token'];
-	res.cookie('dissolutionToken', accessToken, { maxAge: 900000, httpOnly: false });
-	res.redirect("/");
+		// If the access token is valid, stash it as a cookie and redirect the user.
+		var accessToken = loginResponse['access_token'];
+		res.cookie('dissolutionToken', accessToken, { maxAge: 900000, httpOnly: false });
+		res.redirect("/");
+
+	// If we were unable to log the user in, notify them.
+	} catch (error) {
+		res.render('login', { error : "\'Unable to login with that username or password.\'" });
+		return;
+	}
 }));
 
-// Handle visitors logging out.
+// Handle visitors logging out by removing the access token.
 app.post("/logout", function (req, res) {
 	res.clearCookie('dissolutionToken');
 	res.redirect('/');
